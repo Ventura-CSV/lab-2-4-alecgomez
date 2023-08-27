879e2a566705ce8110d32357dcc0d26889eb3fb8
alecgomez <alecgomez0987@gmail.com>
2023-08-26 19:34:08 -0700

    Lab 2-4, 2nd att

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ def main():
    
     original_str = "Python Programming"
 
-    sub2 = original_str[:6]
+    sub1 = original_str[0:6]
 
-    sub1 = original_str[7:]
+    sub2 = original_str[7:18]
 
     merge_str = sub2 + " " + sub1
 
-    print(sub1)
     print(sub2)
+    print(sub1)
     print(merge_str)
 
 pass
