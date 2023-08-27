3000f68a47e87bcc10d8e5f82bdfacbda082788b
alecgomez <alecgomez0987@gmail.com>
2023-08-26 19:30:08 -0700

    Lab 2-4

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+   
+    original_str = "Python Programming"
 
+    sub2 = original_str[:6]
+
+    sub1 = original_str[7:]
+
+    merge_str = sub2 + " " + sub1
+
+    print(sub1)
+    print(sub2)
+    print(merge_str)
+
+pass
 
 if __name__ == '__main__':
     main()
